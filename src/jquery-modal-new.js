1b81a5b1e2f2b27c78a6ddb43b07028df43adc48
joeylin <joe.y.lin.2012@gmail.com>
2013-11-11 13:31:53 +0800

    add css3 transition

--- src/jquery-modal-new.js
+++ src/jquery-modal-new.js
@@ -8,12 +8,38 @@
 
 (function(window, document, $, undefined) {
 
+	var CrossBroswer = (function(){
+		var el = document.createElement('fakeelement'),
+            transitions = {
+                transition:         'transitionend',
+                WebkitTransition:   'webkitTransitionEnd'
+            },
+            css3Support = false,
+            transition;
+
+        // Check transition.
+        for( var t in transitions ) {
+            if ( transitions.hasOwnProperty(t) && el.style[t] !== undefined ) {
+                transition =  transitions[t];
+            }
+        }
+
+		if (transition) {
+			css3Support = true;
+		}
+		return {
+			css3Support: css3Support,
+			animationEnd: transition
+		};
+	})();
+
 	var Modal = $.modal = function(element, options) {
 		this.element = element;
 		this.$element = $(element);
 		this.$overlay = $('<div></div>').css({display: 'none'});
 		this.$container = $('<div></div>').css({display: 'none'});
 		this.$contentWrap = $('<div></div>').appendTo(this.$container);
+		this.$content = $('<div></div>').appendTo(this.$contentWrap);
 
 		// options
         var meta_data = [];
@@ -26,14 +52,24 @@
         this.options = $.extend(true, {}, Modal.defaults, options, meta_data);
         this.namespace = this.options.namespace;
 
+		this.css3Support = CrossBroswer.css3Support;
+		this.animationEnd = CrossBroswer.animationEnd;
+		
+		// switch to jquery animate if not support
+		if (!this.css3Support) {
+			this.options.effect = this.options.jqAnimate;
+		}
+
         this.classes = {
 			overlay: this.namespace + '-overlay',
 			container: this.namespace + '-container',
 			content: this.namespace + '-content',
+			contentWrap: this.namespace + '-contentWrap',
 			skin: this.namespace + '_' + this.options.skin,
 			error: this.namespace + '_' + 'error',
 			open: this.namespace + '_' + 'open',
-			animate: this.namespace + '_animate',
+			effect: this.namespace + '_' + this.options.effect,
+			overlayEffect: this.namespace + '_ovrelay_' + this.options.effect,
 			loading: this.namespace + '_loading'
         };
 
@@ -45,11 +81,13 @@
         }
         this.$container.addClass(this.classes.container);
         this.$overlay.addClass(this.classes.overlay);
-        this.$contentWrap.addClass(this.classes.content);
+        this.$contentWrap.addClass(this.classes.contentWrap);
+        this.$content.addClass(this.classes.content);
 
         this.isLoading = false;
         this.enabled = true;
         this.isError = false;
+        this.isOpen = false;
 
         this.init();
 	};
@@ -57,36 +95,27 @@
 	Modal.prototype = {
 		constructor: Modal,
 		init: function() {
-			var self = this,
-				animate = this._animationSupport();
+			var self = this;
 
 			// check element href
 			if (this.options.content === null) {
 				this.options.content = this.$element.attr('href');
 			}
 
-			// mock
-			this.css3Support = animate.css3Support || false;
-			this.animationEnd = animate.animationEnd || 'animationend';
+			// add animation effect
+			this.$contentWrap.addClass(this.classes.effect);
+			this.$overlay.addClass(this.classes.overlayEffect);
 
 			this.$container.appendTo('body');
 			this.$overlay.appendTo('body');
 
 			// set fixed width/height
 			if (this.options.width) {
-				this.$contentWrap.width(this.options.width);
+				this.$content.width(this.options.width);
 			}
 			if (this.options.height) {
-				this.$contentWrap.height(this.options.height);
+				this.$content.height(this.options.height);
 			}
-
-			this.$contentWrap.on(this.animationEnd, function() {
-				if (typeof self.options.onComplete === 'function') {
-					self.options.onComplete.call(this,this);
-				}
-				self.$container.trigger('modal::onComplete', this);
-				return false;
-			});
 
 			this.$element.on('click.modal', function() {
 				if (self.enabled) {
@@ -129,14 +158,6 @@
 			}
 			return dtd.promise();
 		},
-		_animationSupport: function() {
-			var css3Support, animationend;
-			// TODO: css3 detect
-			return {
-				css3Support: css3Support,
-				animationend: animationend
-			};
-		},
 		_unbindeEvent: function() {
 			if (this.options.closeElement) {
 				this.$close.off('click.modal');
@@ -173,7 +194,7 @@
 				this._hideLoading();
 			}			
 			if (this.options.overlay) {
-				this.$overlay.css({display: 'block'});
+				this.$overlay.css({display: 'block',opacity: 0});
 			}
 			if (this.options.closeByEscape) {
 				$(document).on('keydown.modal', function(event) {
@@ -184,26 +205,26 @@
 				});
 			}
 
-			if (this.$content && !this.isError) {
+			if (this.content && !this.isError) {
 				// prevent reloading
 				this._afterOpen();
 				return;
 			}
 
 			// clear last open info before load
-			this.$contentWrap.removeClass(this.classes.error);
+			this.$content.removeClass(this.classes.error);
 
 			this._load().always(function() {
 				self._hideLoading();
-			}).then(function($content) {
-				self.$content = $content;
-				self.$contentWrap.empty().html(self.$content);
+			}).then(function(content) {
+				self.content = content;
+				self.$content.empty().html(self.content);
 				self._afterOpen();
 			}, function(error) {
-				self.$contentWrap.addClass(self.classes.error);
+				self.$content.addClass(self.classes.error);
 				self.isError = true;
-				self.$content = error;
-				self.$contentWrap.empty().html(self.$content);
+				self.content = error;
+				self.$content.empty().html(self.content);
 				self._afterOpen();
 			});
 		},
@@ -212,20 +233,31 @@
 
 			// this must do after content loaded
 			if (this.options.closeElement) {
-				this.$close = this.$content.find(this.options.closeElement);
+				this.$close = this.content.find(this.options.closeElement);
 				this.$close.on('click.modal', $.proxy(this.hide, this));
 			}
 
 			// show container
-			this.$container.css({display: 'block'}).addClass(this.classes.open);
+			this.$container.css({display: 'block'});
+			
+			// active css3 comeIn animation , if browser doesn't support, just ignore it 
+			// give some space for css3 animation
+			setTimeout(function(){
+				self.$contentWrap.addClass(self.classes.open);
+				self.$overlay.addClass(self.classes.open);
+			}, 0);
 
-			// active css3 comeIn animation , if browser supports 
-			this.$contentWrap.addClass(this.classes.animate);
+			// overlay use jquery animation
+			this.$overlay.animate({opacity:1},{
+				duration: this.options.overlaySpeed
+			});
 
 			// for animation 
 			this.status = 'moveIn';
 			this._animate().then(function() {
-				self.$contentWrap.trigger(self.animationEnd, self);
+				// trigger jquery animation end event manually
+				self.isOpen = true;
+				self.$content.trigger(self.animationEnd, self);
 			});
 		},
 		close: function() {
@@ -234,14 +266,37 @@
 			// for jquery animation 
 			this.status = 'moveOut';
 
+			// css3 animationend event listener
+			this.$content.on(this.animationEnd, function() {
+				self._afterClose();
+				self.$content.off(self.animationEnd)
+				if (typeof self.options.onComplete === 'function') {
+					self.options.onComplete.call(this,this);
+				}
+				self.$container.trigger('modal::onComplete', this);
+				return false;
+			});
+
+			// overlay use jquery animation
+			this.$overlay.animate({opacity: 0},{
+				duration: this.options.overlaySpeed,
+				complete: function() {
+					self.$overlay.css({display:'none'});
+				}
+			});
+
 			// active css3 comeOut animation 
-			this.$contentWrap.removeClass(this.classes.animate);
+			this.$contentWrap.removeClass(this.classes.open);
+			//this.$overlay.removeClass(this.classes.open);
 
 			this._animate().then(function() {
-				self._unbindeEvent();
-				self.$overlay.css({display: 'none'});
-				self.$container.removeClass(self.classes.open).css({display: 'none'});
+				self._afterClose();
 			});
+		},
+		_afterClose: function() {
+			this._unbindeEvent();
+			this.$container.removeClass(this.classes.open).css({display: 'none'});
+			this.isOpen = false;
 		},
 		enable: function() {
 			this.enabled = true;
@@ -262,7 +317,7 @@
 		fade: {
 			moveIn: function(instance, dtd) {
 
-				instance.$contentWrap.animate({
+				instance.$content.animate({
 					opacity: 1
 				},{
 					duration: 400,
@@ -274,7 +329,7 @@
 			},
 			moveOut: function(instance, dtd) {
 
-				instance.$contentWrap.animate({
+				instance.$content.animate({
 					opacity: 0
 				}, {
 					duration: 400,
@@ -293,7 +348,8 @@
 		content: null, // Set the URL, ID or Class.
         overlay: true, // Show the overlay.
         closeElement: null, // Element ID or Class to close the modal
-        effect: 'fade', // fadein | slide | newspaper | fall 
+        effect: 'fadeScale', // fadein | slide | newspaper | fall 
+        overlaySpeed: 200, // Sets the speed of the overlay, in milliseconds
         jqAnimate: 'fade', // set default jquery animate when css3 animation doesn't support
         focus: true, // set focus to form element in content
         errorText: 'sorry, cant find the file !', // set ajax error text
